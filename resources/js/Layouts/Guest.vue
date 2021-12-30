2159176ff06b858f973ba250a6b0140cc8c1ecca
Mateusz Gostański <mateusz.gostanski@gmail.com>
2021-12-30 10:51:45 +0100

    feat: added extra slot in GuestLayout

--- resources/js/Layouts/Guest.vue
+++ resources/js/Layouts/Guest.vue
@@ -9,6 +9,10 @@
         <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
             <slot />
         </div>
+
+        <div class="w-full sm:max-w-md mt-6 text-gray-700 text-sm text-center">
+            <slot name="extra" />
+        </div>
     </div>
 </template>
 
