7ea629155ffd8e3b5386a027907e15fdeb0ba7c1
Mateusz Gostański <mateusz.gostanski@gmail.com>
2022-01-07 16:37:30 +0100

    feat: updated group types

--- src/Domain/Auth/Enums/GroupTypeEnum.php
+++ src/Domain/Auth/Enums/GroupTypeEnum.php
@@ -5,4 +5,12 @@ namespace Domain\Auth\Enums;
 enum GroupTypeEnum: int
 {
     case SERVICE = 1;
+    case ELDERS = 2;
+    case MINISTERIAL_SERVANTS = 3;
+    case AUDIO_VIDEO = 4;
+    case ATTENDANTS = 5;
+    case KHOC = 6;
+    case MAINTENANCE = 7;
+    case WORKGROUP = 8;
+    case SPECIAL = 9;
 }
