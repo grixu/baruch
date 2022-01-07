d8d67d153fc21159fddf12af6cfb12d05a3829eb
Mateusz Gostański <mateusz.gostanski@gmail.com>
2022-01-07 09:57:12 +0100

    feat: created UserLeaveGroup notification

---
+++ src/Domain/Auth/Notifications/UserLeaveGroup.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Domain\Auth\Notifications;
+
+use Domain\Auth\Models\Group;
+use Domain\Auth\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use function url;
+
+class UserLeaveGroup extends Notification
+{
+    use Queueable;
+
+    public function __construct(public User $user, public Group $group)
+    {
+    }
+
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
